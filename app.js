0538fe857a4dcdfd8b02d04bc61c26460c1db5e4
soyoung-choi <sydesign24@naver.com>
2022-06-21 01:31:21 +0900

    초기 세팅

---
+++ app.js
@@ -0,0 +1,67 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
+
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+
+const app = express()
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+)
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET, // 서명에 필요한 값
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60,
+      secure: false, // https일 때 적용
+    },
+    store: new FileStore(),
+  })
+)
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
