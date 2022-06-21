e05bc7ef392f013e3e5c064520e530206c8381c7
soyoung-choi <sydesign24@naver.com>
2022-06-21 12:38:00 +0900

    배포준비

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
 const session = require('express-session')
-const FileStore = require('session-file-store')(session)
+const helmet = require('helmet')
+const hpp = require('hpp')
+const csrf = require('csurf')
+const logger = require('./logger')
+const redis = require('redis')
+const RedisStore = require('connect-redis')(session)
+
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+})
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -25,25 +35,39 @@ app.use(
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.use(logger('dev'))
+if (process.env.NODE_ENV === 'production') {
+  app.enable('trust proxy')
+  app.use(morgan('combined'))
+  app.use(helmet({ contentSecurityPolicy: false }))
+  app.use(hpp())
+} else {
+  app.use(morgan('dev'))
+}
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use(csrf({ cookie: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET, // 서명에 필요한 값
-    cookie: {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60,
-      secure: false, // https일 때 적용
-    },
-    store: new FileStore(),
-  })
-)
+const sessionOption = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET, // 서명에 필요한 값
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60,
+    secure: false, // https일 때 적용
+  },
+  store: new RedisStore({ client: redisClient }),
+}
+
+if (process.env.NODE_ENV === 'production') {
+  sessionOption.proxy = true
+  // sessionOption.cookie.secure = true
+}
+
+app.use(session(sessionOption))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
@@ -56,6 +80,9 @@ app.use(function (req, res, next) {
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
+  logger.error(err)
+  if (err.code !== 'EBADCSRFTOKEN') return next(err)
+
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
